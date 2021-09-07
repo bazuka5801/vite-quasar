a5da9914e532142d6e7669d6feda941557e6cfca
Jeff Galbraith <galbraith64@gmail.com>
2021-09-07 14:21:30 -0600

    feat: add quasar component QBtn and Notify plugin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld msg="Hello Vue 3 + Vite + Quasar" />
 </template>
 
 <script setup>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,8 @@
 
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a> |
+    <a href="https://quasar.dev/" target="_blank">Quasar 2 Documentation</a>
   </p>
 
   <button @click="state.count++">count is: {{ state.count }}</button>
@@ -11,16 +12,27 @@
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <q-btn @click="showNotify">Quasar button</q-btn>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive, onMounted } from 'vue'
+import { useQuasar } from 'quasar'
 
 defineProps({
   msg: String
 })
 
 const state = reactive({ count: 0 })
+const quasar = useQuasar()
+
+onMounted(() => {
+  showNotify()
+})
+
+function showNotify () {
+  quasar.notify('Quasar inside Vite')
+}
 </script>
 
 <style scoped>
